2edb2db3731ea6ba5c4324c8a9072c41aeb36155
Tran Phuoc Tin <tptin.76@gmail.com>
2024-07-28 01:36:10 +0700

    refactor

--- src/ECS/Components/CircleCollider2D.cpp
+++ src/ECS/Components/CircleCollider2D.cpp
@@ -1,6 +1,7 @@
 #include "CircleCollider2D.h"
-
+#include <iostream>
 CircleCollider2D::CircleCollider2D()
 {
-
+    this->setName(__FUNCTION__);
+    std::cout << __FUNCTION__ << ":" << this->name() << std::endl;
 }
--- src/Script/Scenes/GameScene/Entities/Player.cpp
+++ src/Script/Scenes/GameScene/Entities/Player.cpp
@@ -3,13 +3,14 @@
 #include <EntityIDs.h>
 #include <Components/Transform2D.h>
 #include <Components/Health.h>
-
+#include <Components/CircleCollider2D.h>
 Player::Player()
 {
     this->setId(EntityIDs::GameScene::PLAYER);
     this->setName("Player");
     this->AddComponent<Transform2D>(glm::vec2{0.0f, 0.0f}, glm::vec2{0.0f, 0.0f}, glm::vec2{0.0f, 0.0f});
     this->AddComponent<Health>(100.0f);
+    this->AddComponent<CircleCollider2D>();
     std::cout << __FUNCTION__ << ":" << this->name() << std::endl;
 }
 
